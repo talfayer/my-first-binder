9d8477d54754798db40c909a49ecc041b1066072
Tal Fayer <talfayer@email.unc.edu>
2025-09-18 10:54:53 -0400

    Update install.R

--- install.R
+++ install.R
@@ -1,9 +1,4 @@
 install.packages("readr")
 install.packages("tidyr")
 install.packages("ggplot2")
-install.packages("tidyverse", dependencies = TRUE)
-install.packages("rmarkdown", dependencies = TRUE)
-install.packages("IRkernel", dependencies = TRUE)
-install.packages("shiny"), dependencies = TRUE)
-
-IRkernel::installspec(user = FALSE)
+install.packages("tidyverse")
